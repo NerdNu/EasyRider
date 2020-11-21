d7d7cadcf3bc2f5d3aa9824269467ca6457cc2d0
PlNG <tempeml@hotmail.com>
2020-11-21 16:37:44 -0500

    Categorize sounds to NEUTRAL to enable muting

--- src/nu/nerd/easyrider/commands/HorseNextExecutor.java
+++ src/nu/nerd/easyrider/commands/HorseNextExecutor.java
@@ -2,6 +2,7 @@ package nu.nerd.easyrider.commands;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.AbstractHorse;
@@ -86,7 +87,7 @@ public class HorseNextExecutor extends ExecutorBase {
         } else {
             player.sendMessage(ChatColor.RED + "That " + Util.entityTypeName(abstractHorse) + " cannot be trained.");
         }
-        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
+        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, SoundCategory.NEUTRAL, 1.0f, 1.0f);
     }
 
     // ------------------------------------------------------------------------
